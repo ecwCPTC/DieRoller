0c160bbf8a4e92b843beaf4d534633e17c970577
Evan Ward <enjoievan@gmail.com>
2022-07-20 19:59:22 -0700

    Add project files.

---
+++ DieRoller/Die.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    public class Die
+    {
+        private static Random _random;
+
+        static Die()
+        {
+            _random = new Random();
+        }
+        public Die()
+        {
+            Roll();
+        }
+        public byte FaceValue { get; private set; }
+        public bool IsHeld { get; set; }
+        public byte Roll()
+        {
+            if (!IsHeld)
+            {
+                Random random = new();
+                byte newValue = (byte)random.Next(1, 7);
+                FaceValue = newValue;
+            }
+            return FaceValue;
+        }
+
+    }
+}
---
+++ DieRoller/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DieRoller;
+Die myDie = new Die();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+myDie.IsHeld = true;
+Console.WriteLine("Holding the die********************");
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+myDie.Roll();
+Console.WriteLine(myDie.FaceValue);
+
+Console.ReadKey();
